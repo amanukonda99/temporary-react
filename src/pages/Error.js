c83c1f8e4a205923130230f2b62eea031d6d399d
Akhil <amanukonda99@gmail.com>
2023-03-28 15:42:10 -0500

    test commit

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,7 +1,14 @@
+import { Link } from 'react-router-dom';
+import Home from './Home';
+
 const Error = () => {
   return (
     <section className='section'>
-      <h2>Error</h2>
+      <h2>404</h2>
+      <p>page not found</p>
+      <Link to='/' className='btn'>
+        back home
+      </Link>
     </section>
   );
 };
---
+++ src/pages/SharedLayout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import StyledNavbar from '../components/StyledNavbar';
+import { Outlet } from 'react-router-dom';
+const SharedLayout = () => {
+  return (
+    <>
+      <StyledNavbar />
+      <Outlet />
+    </>
+  );
+};
+
+export default SharedLayout;
